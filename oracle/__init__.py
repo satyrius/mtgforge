0b9b5e8be3508e38db1fc61dde34f89d69ba4e37
Anton Egorov <anton.egoroff@gmail.com>
2012-01-23 02:01:20 +0400

    Start Oracle application

---
+++ oracle/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is a mirror for the Oracle, a database of every printed Magic card and
+its current wording. Original data can be found using the Gatherer card
+database at http://gatherer.wizards.com
+"""
