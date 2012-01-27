30cff4b5bd97a4ca2bb67ceff303378c365896a9
Anton Egorov <anton.egoroff@gmail.com>
2012-01-27 11:34:08 +0400

    Fetch card sets' acronyms from magiccards info

---
+++ contrib/utils.py
@@ -0,0 +1,37 @@
+from django.conf import settings
+from django.utils import translation
+from hashlib import sha1
+from django.core.cache import cache as _djcache
+
+
+def translation_aware(func):
+    def wrapper(self, *args, **kwargs):
+        translation.activate(settings.MODELTRANSLATION_DEFAULT_LANGUAGE)
+        func(self, *args, **kwargs)
+        translation.deactivate()
+    return wrapper
+
+def cache(f, seconds = 900):
+    """
+        Cache the result of a function call for the specified number of seconds,
+        using Django's caching mechanism.
+        Assumes that the function never returns None (as the cache returns None to indicate a miss), and that the function's result only depends on its parameters.
+        Note that the ordering of parameters is important. e.g. myFunction(x = 1, y = 2), myFunction(y = 2, x = 1), and myFunction(1,2) will each be cached separately.
+
+        http://djangosnippets.org/snippets/564/
+
+        Usage:
+
+        @cache(600)
+        def myExpensiveMethod(parm1, parm2, parm3):
+            ....
+            return expensiveResult
+    """
+    def wrapper(*args, **kwargs):
+        key = sha1(str(f.__module__) + str(f.__name__) + str(args) + str(kwargs)).hexdigest()
+        result = _djcache.get(key)
+        if result is None:
+            result = f(*args, **kwargs)
+            _djcache.set(key, result, seconds)
+        return result
+    return wrapper
--- oracle/management/commands/fetch_sets.py
+++ oracle/management/commands/fetch_sets.py
@@ -1,20 +1,42 @@
-from django.core.management.base import BaseCommand, CommandError
-from contrib.commands import translation_aware
+from django.core.management.base import BaseCommand, CommandError, smart_str
+from contrib.utils import translation_aware, cache
 from BeautifulSoup import BeautifulSoup
 from contrib.soupselect import select
-from urllib2 import urlopen
+from urllib2 import urlopen, HTTPError
+from django.utils.translation import get_language
+import re
 
 GATHERER_INDEX_PAGE = 'http://gatherer.wizards.com/Pages/Default.aspx'
+MAGICARDS_SITEMAP_PAGE = 'http://magiccards.info/sitemap.html'
+
+acronym_pattern = '[a-z0-9]+'
 
 class Command(BaseCommand):
     def writeln(self, message):
-        self.stdout.write(message + u'\n')
+        self.stdout.write(u'{0}\n'.format(message))
 
-    @translation_aware
-    def handle(self, *args, **options):
-        doc = BeautifulSoup(urlopen(GATHERER_INDEX_PAGE))
+    def notice(self, message):
+        colorized_message = self.style.NOTICE(u'Notice: {0}\n'.format(message))
+        self.stderr.write(smart_str(colorized_message))
+
+    def urlopen(self, url):
+        try:
+            f = urlopen(url)
+            return f
+        except HTTPError, e:
+            raise CommandError(u'{0} {1} {2}'.format(url, e.code, e.msg))
+
+    @cache
+    def soup(self, url):
+        """Fetch url and return BeautifulSoup for the document"""
+        return BeautifulSoup(self.urlopen(url))
+
+    def names(self):
+        """Card sets names generator which retrieves data from Gatherer's page
+        """
+        soup = self.soup(GATHERER_INDEX_PAGE)
         select_id = 'ctl00_ctl00_MainContent_Content_SearchControls_setAddText'
-        options = select(doc, 'select#{0} option'.format(select_id))
+        options = select(soup, 'select#{0} option'.format(select_id))
         if not options:
             raise CommandError(
                 u'Cannot find card set select box #{0} on Gatherer\'s index page {1}'.format(
@@ -22,13 +44,39 @@ class Command(BaseCommand):
                 )
             )
         for o in options:
-            set_name = None
-            for attr_name, attr_value in o.attrs:
-                if attr_name == 'value':
-                    set_name = attr_value
-                    break
+            set_name = o.get('value')
             if set_name is None:
                 raise CommandError(u'Option {0} does not have value attribute'.format(o))
             if not set_name:
                 continue
-            self.writeln(set_name)
+            yield set_name
+
+    def acronym(self, name):
+        """Returns acronym for given name. First it tries to get it from
+        magiccards.info sitemap (because they are pretty), but it asks user
+        input it not found"""
+        soup = self.soup(MAGICARDS_SITEMAP_PAGE)
+
+        acronym_re = re.compile(r'/(?P<acronym>{0})/{1}\.html$'.format(
+            acronym_pattern, get_language()
+        ))
+        match_en_acronym = lambda el: el.parent.name == 'a' and acronym_re.search(el.parent.get('href'))
+
+        links = soup.findAll(text=re.compile(r'^{0}$'.format(name), re.IGNORECASE))
+        links = filter(match_en_acronym, links)
+        acronym = None
+        if not links:
+            while not acronym:
+                acronym = raw_input('Acronym for "{0}": '.format(name)).strip()
+                if not re.match(r'{0}$'.format(acronym_pattern), acronym):
+                    self.notice('Acronym should by alpha-numeric identifier')
+                    acronym = None
+        else:
+            acronym = match_en_acronym(links[0]).group('acronym')
+        return acronym
+
+    @translation_aware
+    def handle(self, *args, **options):
+        for name in self.names():
+            acronym = self.acronym(name)
+            self.writeln(u'{0} ({1})'.format(name, acronym))
