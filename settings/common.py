f0ad4376cf67f825cddfb67d7741e94231c97cf1
Anton Egorov <anton.egoroff@gmail.com>
2012-04-15 09:18:30 +0400

    Update model translation and reconfigure static and media

--- settings/common.py
+++ settings/common.py
@@ -50,18 +50,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(DIR_NAME, 'static')
+MEDIA_ROOT = os.path.join(DIR_NAME, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/tmp/mtg-static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -77,7 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    MEDIA_ROOT,
+    os.path.join(DIR_NAME, 'static'),
 )
 
 # List of finder classes that know how to find static files in
