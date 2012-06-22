ef405b98c9a116cc88cee0a1ca799d07929f59dc
Anton Egorov <anton.egoroff@gmail.com>
2012-06-23 01:44:49 +0400

    card sets resource

--- forge/urls.py
+++ forge/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import patterns, include
 from tastypie.api import Api
 from .resources.card import CardResource
+from .resources.card_set import CardSetResource
+
 
 v1_api = Api(api_name='v1')
 v1_api.register(CardResource())
+v1_api.register(CardSetResource())
 
 urlpatterns = patterns(
     'django.views.generic.simple',
