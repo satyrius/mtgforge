20623a067b601102a4069398a8cb1dc2ff8b24e0
Anton Egorov <anton.egoroff@gmail.com>
2012-08-31 16:20:42 +0400

    list sets command

---
+++ oracle/management/commands/list_sets.py
@@ -0,0 +1,19 @@
+from optparse import make_option
+
+from oracle.management.base import BaseCommand
+from oracle.models import CardSet
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('-s', '--sort', dest='sort', default='name',
+                    help='Sorting method: name, acronym, release '
+                         '[default %default]'),
+    )
+
+    def handle(self, *args, **options):
+        objects = CardSet.objects.all()
+        sorting = dict(name=['name'], acronym=['acronym'], release=['released_at', 'name'])
+        sort = sorting.get(options['sort'], sorting['name'])
+        for cs in objects.order_by(*sort):
+            self.writeln('{:<6} {}'.format(cs.acronym, cs.name))
