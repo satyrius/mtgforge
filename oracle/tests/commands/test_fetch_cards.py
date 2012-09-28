9359f80d68ebdee6368c8ed34b67716550873ef9
Anton Egorov <anton.egoroff@gmail.com>
2012-09-28 17:25:23 +0400

    little test_fetch_card setup improvement

--- oracle/tests/commands/test_fetch_cards.py
+++ oracle/tests/commands/test_fetch_cards.py
@@ -10,9 +10,9 @@ from oracle.tests.providers.base import ProviderTest
 
 class FetchCardsCommandTest(ProviderTest):
     @patch.object(GathererCard, 'get_content')
-    def test_save_card_oracle_details(self, get_content):
-        cs = CardSet.objects.create(name='Avacyn Restored')
-        self.assertEqual(cs.cardrelease_set.all().count(), 0)
+    def setUp(self, get_content):
+        self.cs = CardSet.objects.create(name='Avacyn Restored')
+        self.assertEqual(self.cs.cardrelease_set.all().count(), 0)
         self.assertEqual(CardFace.objects.all().count(), 0)
         self.assertEqual(Card.objects.all().count(), 0)
 
@@ -20,15 +20,16 @@ class FetchCardsCommandTest(ProviderTest):
         url = 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=239961'
         name = u'Avacyn, Angel of Hope'
         page = GathererCard(url, name=name)
-        details = page.details()
+        self.details = page.details()
 
+    def test_save_card_oracle_details(self):
         cmd = Command()
         self.assertFalse(cmd.no_update)
 
-        card_face = cmd.save_card_face(details, cs)
+        card_face = cmd.save_card_face(self.details, self.cs)
         card = card_face.card
         self.assertIsInstance(card_face, CardFace)
 
-        release = card.cardrelease_set.get(card_set=cs)
+        release = card.cardrelease_set.get(card_set=self.cs)
         self.assertEqual(release.rarity, CardRelease.MYTHIC)
         self.assertEqual(release.card_number, 6)
