9d114764aa5520a7363f62f98a8a961d49638a4e
Anton Egorov <anton.egoroff@gmail.com>
2012-10-04 21:24:47 +0400

    Card admin improvement

--- oracle/admin/card.py
+++ oracle/admin/card.py
@@ -4,13 +4,16 @@ from oracle.admin.card_face import CardFaceInline
 
 
 class CardReleaseInline(admin.TabularInline):
+    ordering = ('card_set__released_at', 'card_set__name', 'card_number',)
     model = models.CardRelease
     extra = 0
 
 
 class CardAdmin(admin.ModelAdmin):
-    readonly_fields = ('name',)
-    search_fields = ('name', 'cardface__cardl10n__name')
     inlines = [CardReleaseInline, CardFaceInline]
+    list_filter = ('cardrelease__card_set__name',)
+    ordering = ('name',)
+    readonly_fields = ('name',)
+    search_fields = ('name', 'cardface__cardl10n__name',)
 
 admin.site.register(models.Card, CardAdmin)
