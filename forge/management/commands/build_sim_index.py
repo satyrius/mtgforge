2a324cb4ce586d495c795285c4d86d7e93f3e204
Anton Egorov <anton.egoroff@gmail.com>
2013-02-08 04:19:59 +0400

    flake8

--- forge/management/commands/build_sim_index.py
+++ forge/management/commands/build_sim_index.py
@@ -6,19 +6,21 @@ from django.db import transaction, connection
 from forge.models import CardSimilarity
 from oracle.models import CardL10n
 
+
 def exists(cur, keyword):
     cur.execute("""
-        SELECT count(*) FROM forge_cardsimilarity 
+        SELECT count(*) FROM forge_cardsimilarity
         WHERE keyword = %s
     """, [keyword])
     return cur.fetchone()[0]
 
+
 class Command(BaseCommand):
 
-    @transaction.commit_on_success    
+    @transaction.commit_on_success
     def handle(self, *args, **options):
-        
-        cursor = connection.cursor()       
+
+        cursor = connection.cursor()
         for card in CardL10n.objects.all():
             keywords = []
             if card.rules:
@@ -27,13 +29,8 @@ class Command(BaseCommand):
             keywords += re.split('[^\w]', card.name, flags=re.IGNORECASE)
             keywords += re.split('[^\w]', card.type_line, flags=re.IGNORECASE)
             for keyword in keywords:
-                if not keyword: continue
+                if not keyword:
+                    continue
                 keyword = keyword.lower()
                 if not exists(cursor, keyword):
                     CardSimilarity(keyword=keyword).save()
-                    
-            
-        
-
-        
-        
