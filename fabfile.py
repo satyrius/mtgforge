7179f9fe4b889d5b466473126fa2c557b3a94195
Anton Egorov <anton.egoroff@gmail.com>
2013-09-18 01:14:55 +0400

    run dev servers using fabric, old fabric tasks are depricated

--- fabfile.py
+++ fabfile.py
@@ -1,50 +1,9 @@
-from fabric.api import env, cd, sudo, prefix, task
-from functools import wraps
-
-
-env.hosts = ['topdeck.pro']
-virtualenv = 'source /var/virtualenv/mtgforge/bin/activate'
-
-
-def project_task(func):
-    @task
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        with cd('/var/www/mtgforge'):
-            with prefix(virtualenv):
-                with prefix('export DJANGO_SETTINGS_MODULE=settings.prod'):
-                    return func(*args, **kwargs)
-    return wrapper
+from fabric.api import local, lcd, task
 
 
 @task
-def restart():
-    sudo('service uwsgi restart')
-    sudo('service nginx restart')
-
-
-@project_task
-def recollect_static():
-    sudo('rm -rf _generated_media*')
-    sudo('./dj.sh generatemedia')
-    sudo('./dj.sh compressmedia')
-    sudo('./dj.sh collectstatic --clear --noinput')
-
-
-@project_task
-def build_fts():
-    sudo('./dj.sh build_fts_index')
-    sudo('./dj.sh build_sim_index')
-    sudo('./dj.sh build_suggest')
-
-
-@project_task
-def deploy():
-    sudo('git reset --hard')
-    sudo('git pull')
-    sudo('find . -name "*.pyc" -delete')
-    sudo('pip install -r requirements.txt')
-    sudo('./dj.sh migrate --merge --delete-ghost-migrations')
-    build_fts()
-    recollect_static()
-    restart()
+def runserver():
+    with lcd('frontend'):
+        local('brunch watch &')
+    with lcd('backend'):
+        local('./manage.py runserver')
--- runserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DIR=$(dirname $0)
-cd $DIR/frontend
-brunch watch &
-../backend/manage.py runserver
