06c9d013ffdd559473c11a7035507cbb89cd1d96
Anton Egorov <anton.egoroff@gmail.com>
2013-09-19 07:48:08 +0400

    fab test with reuse db

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,20 @@
-from fabric.api import local, lcd, task
+from fabric.api import local, lcd, task, prefix
 import deploy
+
+
+def lmanage(command):
+    with lcd('backend'):
+        local('./manage.py %s' % command)
 
 
 @task(alias='runs')
 def runserver():
     with lcd('frontend'):
         local('brunch watch &')
-    with lcd('backend'):
-        local('./manage.py runserver')
+    lmanage('runserver')
+
+
+@task
+def test():
+    with prefix('export REUSE_DB=1'):
+        lmanage('test')
