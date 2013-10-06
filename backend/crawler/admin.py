19272736a2d2f13ede335691311a3662a51f9957
Anton Egorov <anton.egoroff@gmail.com>
2013-10-06 17:01:01 +0400

    do not allow add/change/delete data source inlines

--- backend/crawler/admin.py
+++ backend/crawler/admin.py
@@ -5,3 +5,9 @@ from crawler.models import DataSource
 class DataSourceInline(generic.GenericTabularInline):
     model = DataSource
     extra = 0
+    readonly_fields = ('provider', 'url',)
+    can_delete = False
+    can_add = False
+
+    def has_add_permission(self, *args, **kwargs):
+        return False
