d5166224ca8e36e182ab68f3350917f1dfcc2e67
Anton Egorov <anton.egoroff@gmail.com>
2014-01-19 22:48:12 +0400

    base card item pipeline

--- backend/crawler/pipelines.py
+++ backend/crawler/pipelines.py
@@ -7,35 +7,39 @@ class Duplicate(DropItem):
     pass
 
 
-class DupsHandlePipeline(object):
+class BaseCarditemPipeline(object):
+    def _process_item(self, item, spider):
+        raise NotImplemented()
+
+    def process_item(self, item, spider):
+        if isinstance(item, CardItem):
+            self._process_item(item, spider)
+        return item
+
+
+class DupsHandlePipeline(BaseCarditemPipeline):
     def __init__(self):
         self.found = []
 
-    def process_item(self, item, spider):
-        if isinstance(item, CardItem):
-            # Check only cars that have siblings (double faced, splited and
-            # fliped cards). Only these cards may have duped because there
-            # more than one link follows to the card page and a card page has
-            # all card faces on on it.
-            sibling = item.get('sibling')
-            number = item.get('number')
-            if sibling and number:
-                key = (item['set'], number)
-                if key in self.found:
-                    raise Duplicate(
-                        '"{}" has already scraped for "{}"'.format(
-                            item['name'], item['set']))
-                else:
-                    self.found.append(key)
-
-        return item
+    def _process_item(self, item, spider):
+        # Check only cars that have siblings (double faced, splited and
+        # fliped cards). Only these cards may have duped because there
+        # more than one link follows to the card page and a card page has
+        # all card faces on on it.
+        sibling = item.get('sibling')
+        number = item.get('number')
+        if sibling and number:
+            key = (item['set'], number)
+            if key in self.found:
+                raise Duplicate(
+                    '"{}" has already scraped for "{}"'.format(
+                        item['name'], item['set']))
+            else:
+                self.found.append(key)
 
 
-class CardsPipeline(object):
-    def process_item(self, item, spider):
-        if isinstance(item, CardItem):
-            key = re.sub('[^a-z0-9]', '_', item['set'].lower())
-            spider.crawler.stats.inc_value(u'card_item_count/{}'.format(key))
-            # Save the card
-            pass
-        return item
+class CardsPipeline(BaseCarditemPipeline):
+    def _process_item(self, item, spider):
+        key = re.sub('[^a-z0-9]', '_', item['set'].lower())
+        spider.crawler.stats.inc_value(u'card_item_count/{}'.format(key))
+        # Save the card
