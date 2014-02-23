bbb3d979eb3e50aadabffff520f289a66a0cf778
Anton Egorov <anton.egoroff@gmail.com>
2014-02-23 18:20:02 +0400

    players crawler

--- crawlers/mtgru/mtgru/items.py
+++ crawlers/mtgru/mtgru/items.py
@@ -1,11 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
-class MtgruItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+
+class PlayerItem(Item):
+    name = Field()
+    file_urls = Field()
+    files = Field()
--- crawlers/mtgru/mtgru/settings.py
+++ crawlers/mtgru/mtgru/settings.py
@@ -1,15 +1,15 @@
-# Scrapy settings for mtgru project
-#
-# For simplicity, this file contains only the most important settings by
-# default. All the other settings are documented here:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#
+from os.path import dirname, join
 
 BOT_NAME = 'mtgru'
 
 SPIDER_MODULES = ['mtgru.spiders']
 NEWSPIDER_MODULE = 'mtgru.spiders'
+
+FILES_STORE = join(dirname(dirname(__file__)), 'files')
+
+ITEM_PIPELINES = {
+    'scrapy.contrib.pipeline.files.FilesPipeline': 1
+}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'mtgru (+http://www.yourdomain.com)'
