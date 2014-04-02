890d69e7b5560ddeef04daaa56568293ec12ea28
Anton Egorov <anton.egoroff@gmail.com>
2014-04-03 01:11:31 +0400

    Update fab commands for dump/restore prod database

--- fabfile/pg.py
+++ fabfile/pg.py
@@ -1,5 +1,5 @@
 from os.path import join
-from fabric.api import task, sudo, get, local, env
+from fabric.api import task, sudo, get, local
 from fabfile.helpers import are_you_sure
 
 
@@ -8,13 +8,12 @@ DUMP_DATA = 'mtgforge.dump.data'
 
 
 @task
-def dump():
-    db = env['database']
+def dump(db='mtgforge', downloads='.'):
     schema_dump = join('/tmp', DUMP_SCHEMA)
     data_dump = join('/tmp', DUMP_DATA)
 
     sudo(' '.join([
-        'sudo -u mtgforge pg_dump -h localhost ',
+        'sudo -u postgres pg_dump',
         '--schema-only',
         '--no-owner',
         '--no-privileges',
@@ -22,7 +21,7 @@ def dump():
         '%s > %s']) % (db, schema_dump))
 
     sudo(' '.join([
-        'sudo -u mtgforge pg_dump -h localhost',
+        'sudo -u postgres pg_dump',
         '--data-only',
         '--disable-triggers',
         '--no-owner',
@@ -30,15 +29,13 @@ def dump():
         '-T django_session',
         '%s > %s']) % (db, data_dump))
 
-    get('/tmp/mtgforge.dump.*', env['downloads'])
+    get('/tmp/mtgforge.dump.*', downloads)
 
 
 @task
-def restore():
-    db = env['database']
-    dl = env['downloads']
+def restore(db, downloads='.'):
     if are_you_sure('This will destroy current database.'):
         local('dropdb %s' % db)
         local('createdb %s' % db)
-        local('psql %s < %s' % (db, join(dl, DUMP_SCHEMA)))
-        local('pg_restore -d mtgforge --format=c %s' % join(dl, DUMP_DATA))
+        local('psql %s < %s' % (db, join(downloads, DUMP_SCHEMA)))
+        local('pg_restore -d %s --format=c %s' % (db, join(downloads, DUMP_DATA)))
