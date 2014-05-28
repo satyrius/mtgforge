a8cea62db08210dace8cbaf860636f1dcbe82439
Anton Egorov <anton.egoroff@gmail.com>
2014-05-29 01:37:17 +0400

    return media url with host for production

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -13,6 +13,10 @@ except IOError:
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 MEDIA_ROOT = '/var/www/mtgforge-media/'
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+MEDIA_URL = 'http://topdeck.pro/media/'
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
