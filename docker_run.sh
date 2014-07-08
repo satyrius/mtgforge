9adfba733ac2b7a1c7c3a1342699ba72b3a17f6d
Anton Egorov <anton.egoroff@gmail.com>
2014-07-08 13:57:13 +0400

    pass database url to docker container

--- docker_run.sh
+++ docker_run.sh
@@ -2,8 +2,12 @@
 PROJECT='mtgforge'
 PREV_ID=`docker ps | grep $PROJECT | awk '{print $1}'`
 [[ -n $PREV_ID ]] && docker stop $PREV_ID 1>/dev/null
-docker build -t=$PROJECT .
-docker run -d -p 2222:22 -p 8000:80 $PROJECT
+docker build --rm -t=$PROJECT .
+
+DB_HOST=`boot2docker config 2>/dev/null | grep HostIP | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'`
+docker run -d \
+    -e DATABASE_URL="postgres://$USER@$DB_HOST/mtgforge" \
+    -p 2222:22 -p 8000:80 $PROJECT
 
 BOOT2DOCKER_IP=`boot2docker ip 2>/dev/null`
 TCP_PORT_MAPPING=`docker ps -l | grep -Eo ':([0-9]{2,5})->80' | grep -Eo '[0-9]{3,5}'`
