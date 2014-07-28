b14ea2aeab324f00e040831fbf259fd160f344d5
Anton Egorov <anton.egoroff@gmail.com>
2014-07-29 02:58:58 +0400

    change docker port and info messages

--- docker_run.sh
+++ docker_run.sh
@@ -7,11 +7,11 @@ docker build --rm -t=$PROJECT .
 DB_HOST=`boot2docker config 2>/dev/null | grep HostIP | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'`
 docker run -d \
     -e DATABASE_URL="postgres://$USER@$DB_HOST/mtgforge" \
-    -p 2222:22 -p 8000:80 $PROJECT
+    -p 1022:22 -p 8000:80 $PROJECT
 
 BOOT2DOCKER_IP=`boot2docker ip 2>/dev/null`
 TCP_PORT_MAPPING=`docker ps -l | grep -Eo ':([0-9]{2,5})->80' | grep -Eo '[0-9]{3,5}'`
 SSH_PORT_MAPPING=`docker ps -l | grep -Eo ':([0-9]{2,5})->22' | grep -Eo '[0-9]{3,5}'`
 
-echo "WEB http://$BOOT2DOCKER_IP:$TCP_PORT_MAPPING"
-echo "SSH ssh -p $SSH_PORT_MAPPING root@$BOOT2DOCKER_IP"
+echo "http://$BOOT2DOCKER_IP:$TCP_PORT_MAPPING"
+echo "ssh -p $SSH_PORT_MAPPING root@$BOOT2DOCKER_IP"
