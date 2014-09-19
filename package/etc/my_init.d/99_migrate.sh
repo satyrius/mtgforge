7ef2bbbca9a18f32e67fca8d95f5df89e9074f95
Anton Egorov <anton.egoroff@gmail.com>
2014-09-19 20:03:11 +0400

    do not run migrations on container start

--- package/etc/my_init.d/99_migrate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-cd /var/www/mtgforge
-setuser www-data ./manage.py syncdb --noinput
-setuser www-data ./manage.py migrate --merge --delete-ghost-migrations --no-initaial-data --noinput -v2 || true
